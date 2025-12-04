996faee0d6b9941e2310088f9b895a8dccfe7ee8
akvanaparthy <vanaparthyakshay@gmail.com>
2025-12-03 19:25:26 -0500

    Added force dynamic to check plus query

--- app/api/auth/check-plus-status/route.ts
+++ app/api/auth/check-plus-status/route.ts
@@ -1,6 +1,8 @@
 import { NextRequest, NextResponse } from 'next/server';
 import { prisma } from '@/lib/db/prisma';
 
+export const dynamic = 'force-dynamic';
+
 export async function GET(request: NextRequest) {
   try {
     const email = request.nextUrl.searchParams.get('email');
